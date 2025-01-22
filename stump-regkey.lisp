2e6dd1dff820447ab62c0cced78de254c72ca9fb
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-01-22 15:26:32 +0100

    Fix for multiple calls to register-* at once

--- stump-regkey.lisp
+++ stump-regkey.lisp
@@ -13,9 +13,22 @@
 (defun change-keyboard-mapping-row (row keycode)
   (xlib:change-keyboard-mapping *display* (list->2d-array (list row)) :first-keycode keycode))
 
+(defvar *used-keycodes* nil)
+
+(defun used-keycode (keycode)
+  (member keycode *used-keycodes* :test #'=))
+
+(defun empty-keycodes ()
+  (let ((empty-keycodes (multiple-value-list (xlib:keysym->keycodes *display* 0))))
+    (remove-if #'used-keycode empty-keycodes)))
+
+(defun empty-keycode ()
+  (first (empty-keycodes)))
+
 (defun register-keysym (keysym)
-  (let ((empty-keycode (xlib:keysym->keycodes *display* 0)))
-    (change-keyboard-mapping-row (list keysym 0 0 0 0 0 0 0 0 0 0 0 0 0 0) empty-keycode)))
+  (let ((empty-keycode (empty-keycode)))
+    (change-keyboard-mapping-row (list keysym 0 0 0 0 0 0 0 0 0 0 0 0 0 0) empty-keycode)
+    (pushnew empty-keycode *used-keycodes*)))
 
 (defun register-character (character)
   (register-keysym (first (xlib:character->keysyms character))))
